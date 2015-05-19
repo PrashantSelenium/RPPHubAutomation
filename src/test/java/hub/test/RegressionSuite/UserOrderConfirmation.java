b147a03d06f4616ee0347155281e988041fcf173
jenn333 <jennifer.maglaque@twistresources.com>
2015-05-19 10:33:06 +0800

    order conf

---
+++ src/test/java/hub/test/RegressionSuite/UserOrderConfirmation.java
@@ -0,0 +1,41 @@
+package hub.test.RegressionSuite;
+
+
+import hub.library.TestInitReference;
+import hub.utilities.RegressionSuiteUtil.UserRegressionSuiteUtil;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import atu.testng.reports.ATUReports;
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+import atu.testng.reports.utils.Utils;
+
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class, MethodListener.class })
+public class UserOrderConfirmation extends TestInitReference {
+	{
+		System.setProperty("atu.reporter.config", "/tooltwist/eclipse/RPPHub/conf/atu.properties");
+	}
+	    
+    @BeforeClass
+    public void init() {
+           ATUReports.setWebDriver(driver);
+           setIndexPageDescription();
+           driver.navigate().to("https://stage-cbalender.rppropertyhub.com/Login");
+    }
+    
+    private void setIndexPageDescription() {
+        ATUReports.indexPageDescription = "Hub";
+        ATUReports.setAuthorInfo("Automation Tester", Utils.getCurrentTime(),"1.0");
+    }
+
+	@Test(description="User - Order Confirmation", priority=0)
+	public void RT_02707_Order_Confirmation_HTTPS() throws Exception {		
+		UserRegressionSuiteUtil util = new UserRegressionSuiteUtil();
+		util.ocHTTPS();
+	}
+
+}
