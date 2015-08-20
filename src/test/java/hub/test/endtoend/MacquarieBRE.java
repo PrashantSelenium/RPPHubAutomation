818a66b15a1297f4db47da76db66d8aa6f24f317
arvintwano <arvin.twano@Skylark.local>
2015-08-20 15:05:37 +0800

    Hub - Jenkins
    Hub - Jenkins

--- src/test/java/hub/test/endtoend/MacquarieBRE.java
+++ src/test/java/hub/test/endtoend/MacquarieBRE.java
@@ -4,33 +4,15 @@ import hub.library.TestInitReference;
 import hub.utilities.RegressionSuiteUtil.UserRegressionSuiteUtil;
 
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import atu.testng.reports.ATUReports;
-import atu.testng.reports.listeners.ATUReportsListener;
-import atu.testng.reports.listeners.ConfigurationListener;
-import atu.testng.reports.listeners.MethodListener;
-import atu.testng.reports.utils.Utils;
-
-@Listeners({ ATUReportsListener.class, ConfigurationListener.class, MethodListener.class })
 public class MacquarieBRE extends TestInitReference {
-	{
-		System.setProperty("atu.reporter.config", ATULocation);
-	}
-	    
+		    
     @BeforeClass
     public void init() {
-           ATUReports.setWebDriver(driver); 
-           setIndexPageDescription();
            driver.navigate().to("https://stage-mbms.rppropertyhub.com/Login");
     }
     
-    private void setIndexPageDescription() {
-        ATUReports.indexPageDescription = "Hub";
-        ATUReports.setAuthorInfo("Automation Tester", Utils.getCurrentTime(),"1.0");
-    }
-
 	@Test(description="User - Login", priority=0)
 	public void Macquarie_product() throws Exception {
 		
